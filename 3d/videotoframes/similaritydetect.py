174caaa4c2268f8a84b083355dd8f2ad10a1ec94
Bosco Han <boscoh@Boscos-MacBook-Pro.local>
2022-02-04 00:44:19 -0500

    adding SIFT frame reduction

--- 3d/videotoframes/similaritydetect.py
+++ 3d/videotoframes/similaritydetect.py
@@ -5,9 +5,6 @@ import math, operator
 import os
 
 
-rms_diff_limit = 27.0
-frames_directory = './frames'; image_name = '/frame'; image_fmt = '.JPG'
-
 def acquireLock():
     ''' acquire exclusive lock file access, creates lock file if not exist '''
     locked_file_descriptor = open('lockfile.LOCK', 'w+')
@@ -30,45 +27,3 @@ def rmsdiff(im1, im2):
         map(lambda h, i: h*(i**2), h, range(256))
     ) / (float(im1.size[0]) * im1.size[1]))
 
-
-def removeSimilarFrames():
-
-    # lock directory
-    lock_fd = acquireLock()
-
-    deleted = 0
-    directory = os.fsencode(frames_directory)
-    images_in_dir_cnt = len(os.listdir(directory))
-
-    # find reference image in directory (frame with the lowest index)
-    starting_idx = 0
-    while starting_idx < images_in_dir_cnt:
-        ref_image_path = frames_directory + image_name + str(starting_idx) + image_fmt
-        if (os.path.isfile(ref_image_path)):
-            break
-        starting_idx +=1 
-
-    reference_image = Image.open(frames_directory + image_name + str(starting_idx) + image_fmt)
-    print("Starting idx: " + str(starting_idx))
-
-    for i in range(starting_idx+1, images_in_dir_cnt):
-        comparing_image_path = frames_directory + image_name + str(i) + image_fmt
-        if (not os.path.isfile(comparing_image_path)):
-            continue
-
-        comparing_image = Image.open(comparing_image_path)
-        diff = rmsdiff(reference_image, comparing_image)
-
-        if (diff > rms_diff_limit):
-            # update the reference being compared to
-            reference_image = comparing_image
-        else :
-            # if too similar, delete the img
-            print("deleteing" + frames_directory + image_name + str(i) + image_fmt)
-            deleted += 1
-            os.remove(comparing_image_path)
-
-
-    print("Number of frames deleted: " + str(deleted))
-    # release write access on directory
-    releaseLock(lock_fd)
--- 3d/videotoframes/videotoframe.py
+++ 3d/videotoframes/videotoframe.py
@@ -1,6 +1,6 @@
 import cv2
 from blurdetect import isblurry
-from similaritydetect import removeSimilarFrames
+from reduceframes import removeSimilarFramesRMS, removeSimilarFramesSIFT
 import shutil
 import os
 
@@ -10,7 +10,6 @@ if os.path.exists(output_directory):
   shutil.rmtree(output_directory)
 
 os.makedirs(output_directory)
-
 
 vidcap = cv2.VideoCapture('./input_videos/input.MOV')
 success,image = vidcap.read()
@@ -26,17 +25,17 @@ while success:
   if pictureblurred == False:
     #cv2.putText(image, "{}: {:.2f}".format('Blur', fm), (10, 30),
 		  #cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 3)
-    cv2.imwrite( './frames/' + "frame%d.jpg" % count, image)     # save frame as JPEG file      
-    print('Read a new frame: ', success)
+    cv2.imwrite(output_directory + '/'+ "frame%d.jpg" % count, image)     # save frame as JPG file      
     count += 1
   else:
-    print ("Frame was deleted")
+    print ("blurry frame was deleted")
 
 
-removeSimilarFrames()
-
+removeSimilarFramesRMS()
+removeSimilarFramesSIFT()
+print("NUMBER OF FRAMES LEFT", len(os.listdir(output_directory)))
 print('Completed video to frame processing, generating model now...')
 
-os.system('./HelloPhotogrammetry ./frames ./output_models/result.usdz -d medium ')
+os.system('./HelloPhotogrammetry {} ./output_models/result.usdz -d medium '.format(output_directory))
 
 print('Completed Model')
