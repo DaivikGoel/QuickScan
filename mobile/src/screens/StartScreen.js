bdb743f0afa48c5d80c4f6d162c676b4d0628db6
Michael Han <michaelhanwchs@gmail.com>
2022-02-08 15:35:26 -0500

    Add mobile skeleton
    includes login page

---
+++ mobile/src/screens/StartScreen.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Background from '../components/Background'
+import Logo from '../components/Logo'
+import Header from '../components/Header'
+import Button from '../components/Button'
+import Paragraph from '../components/Paragraph'
+
+export default function StartScreen({ navigation }) {
+  return (
+    <Background>
+      <Logo />
+      <Header>QuickScan</Header>
+      <Paragraph>
+        Create 3D objects on the fly.
+      </Paragraph>
+      <Button
+        mode="contained"
+        onPress={() => navigation.navigate('LoginScreen')}
+      >
+        Login
+      </Button>
+      <Button
+        mode="outlined"
+        onPress={() => navigation.navigate('RegisterScreen')}
+      >
+        Sign Up
+      </Button>
+    </Background>
+  )
+}
