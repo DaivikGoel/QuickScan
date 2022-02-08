5036d91d6061c7bdf341e9e33c57daad9fe66dce
Michael Han <michaelhanwchs@gmail.com>
2022-02-08 17:19:33 -0500

    firebase login progress
    firebase connected just need to write login logic

--- mobile/src/screens/Dashboard.js
+++ mobile/src/screens/Dashboard.js
@@ -9,10 +9,9 @@ export default function Dashboard({ navigation }) {
   return (
     <Background>
       <Logo />
-      <Header>Let’s start</Header>
+      <Header>Welcome</Header>
       <Paragraph>
-        Your amazing app starts here. Open you favorite code editor and start
-        editing this project.
+        Logged in.
       </Paragraph>
       <Button
         mode="outlined"
--- mobile/src/screens/LoginScreen.js
+++ mobile/src/screens/LoginScreen.js
@@ -10,6 +10,40 @@ import BackButton from '../components/BackButton'
 import { theme } from '../core/theme'
 import { emailValidator } from '../helpers/emailValidator'
 import { passwordValidator } from '../helpers/passwordValidator'
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAidQ4LnvFBCBU6Q1VbTPVivfJZ_E6APbQ",
+  authDomain: "quickscan-2f853.firebaseapp.com",
+  projectId: "quickscan-2f853",
+  storageBucket: "quickscan-2f853.appspot.com",
+  messagingSenderId: "361465475773",
+  appId: "1:361465475773:web:45dc4aace1a0962ffbbdd4",
+  measurementId: "G-V91KRP3H7K"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const auth = getAuth();
+
+function loginValidator(email, password) {
+  signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+    return 'loggedin'
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    return "Incorrect email or password."
+  });
+}
 
 export default function LoginScreen({ navigation }) {
   const [email, setEmail] = useState({ value: '', error: '' })
@@ -18,9 +52,16 @@ export default function LoginScreen({ navigation }) {
   const onLoginPressed = () => {
     const emailError = emailValidator(email.value)
     const passwordError = passwordValidator(password.value)
+    const loginError = loginValidator(email.value, password.value)
+    console.log(loginError)
     if (emailError || passwordError) {
       setEmail({ ...email, error: emailError })
       setPassword({ ...password, error: passwordError })
+      return
+    }
+    if (typeof loginError == undefined || loginError == null) {
+      setEmail({ ...email, error: "trash" })
+      setPassword({ ...password, error: "garbage" })
       return
     }
     navigation.reset({
