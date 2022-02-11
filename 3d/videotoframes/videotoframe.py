dae8017296e34765018bde6f86486c96a045988c
Daivik Goel <daivikgoel98@gmail.com>
2022-02-10 19:10:30 -0500

    Added Flask to Project

--- 3d/videotoframes/videotoframe.py
+++ 3d/videotoframes/videotoframe.py
@@ -4,38 +4,43 @@ from reduceframes import removeSimilarFramesRMS, removeSimilarFramesSIFT
 import shutil
 import os
 
-output_directory = './frames'
+def videotoframe():
+  output_directory = './frames'
 
-if os.path.exists(output_directory):
-  shutil.rmtree(output_directory)
+  if os.path.exists(output_directory):
+    shutil.rmtree(output_directory)
 
-os.makedirs(output_directory)
+  os.makedirs(output_directory)
 
-vidcap = cv2.VideoCapture('./input_videos/input.MOV')
-success,image = vidcap.read()
-count = 0
-
-while success:
+  vidcap = cv2.VideoCapture('./input_videos/input.MOV')
   success,image = vidcap.read()
+  count = 0
 
-  if image is None:
-    continue
+  while success:
+    success,image = vidcap.read()
 
-  pictureblurred, fm = isblurry(image)
-  if pictureblurred == False:
-    #cv2.putText(image, "{}: {:.2f}".format('Blur', fm), (10, 30),
-		  #cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 3)
-    cv2.imwrite(output_directory + '/'+ "frame%d.jpg" % count, image)     # save frame as JPG file      
-    count += 1
-  else:
-    print ("blurry frame was deleted")
+    if image is None:
+      continue
+
+    pictureblurred, fm = isblurry(image)
+    if pictureblurred == False:
+      #cv2.putText(image, "{}: {:.2f}".format('Blur', fm), (10, 30),
+        #cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 3)
+      cv2.imwrite(output_directory + '/'+ "frame%d.jpg" % count, image)     # save frame as JPG file      
+      count += 1
+    else:
+      print ("blurry frame was deleted")
 
 
-removeSimilarFramesRMS()
-removeSimilarFramesSIFT()
-print("NUMBER OF FRAMES LEFT", len(os.listdir(output_directory)))
-print('Completed video to frame processing, generating model now...')
+  removeSimilarFramesRMS()
+  removeSimilarFramesSIFT()
+  print("NUMBER OF FRAMES LEFT", len(os.listdir(output_directory)))
+  print('Completed video to frame processing, generating model now...')
 
-os.system('./HelloPhotogrammetry {} ./output_models/result.usdz -d medium '.format(output_directory))
+  os.system('./HelloPhotogrammetry {} ./output_models/result.usdz -d medium '.format(output_directory))
 
-print('Completed Model')
+  print('Completed Model')
+
+if __name__ == "__videotoframe__":
+   # stuff only to run when not called via 'import' here
+   videotoframe()
