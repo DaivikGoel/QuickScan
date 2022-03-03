ece055d5e5bed38031d09734942bd47e253b14e9
Michael Han <michaelhanwchs@gmail.com>
2022-03-03 16:22:05 -0500

    Add aws download file to computer
    -downloads 3d object to computer when pressing button on the dashboard

--- web/src/pages/dashboard.tsx
+++ web/src/pages/dashboard.tsx
@@ -6,8 +6,62 @@ import Col from '@paljs/ui/Col';
 import { InputGroup } from '@paljs/ui/Input';
 import styled from 'styled-components';
 import { Card, CardBody, CardFooter } from '@paljs/ui/Card';
+import AWS from 'aws-sdk';
+import { Button } from '@paljs/ui/Button';
+import axios from 'axios';
 
 const Home = () => {
+  // yolo
+  AWS.config.update({
+    accessKeyId: 'AKIA4RGMXYINS5GS6JIA',
+    secretAccessKey: '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',
+    region: 'ca-central-1'
+  });
+
+  const requestUrl = 'http://ec2-3-98-130-154.ca-central-1.compute.amazonaws.com:3000/collection'
+  axios.get(requestUrl)
+  .then(function (response) {
+    const objects = response.data.data
+    console.log(response.data.data[1]["three_dimen_object_blob_storage"])
+    console.log(objects)
+    //console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+
+  const handleDownload = () => {
+    const fileName = '1215.usdz'
+    const s3 = new AWS.S3();
+
+    const params = {
+      Bucket: 'quick-scan-3d-objects',
+      Key: fileName,
+    };
+
+    const signedUrl = s3.getSignedUrl('getObject', params)
+    const link = document.createElement('a');
+    // Set link's href to point to the Blob URL
+    link.href = signedUrl;
+    link.download = fileName;
+    // Append link to the body
+    document.body.appendChild(link);
+    // Dispatch click event on the link
+    // This is necessary as link.click() does not work on the latest firefox
+    link.dispatchEvent(
+      new MouseEvent('click', {
+        bubbles: true,
+        cancelable: true,
+        view: window,
+      })
+    );
+
+    // Remove link from body
+    document.body.removeChild(link);
+}
+
+
   const FilterStyle = styled.div`
     margin: 1rem;
   `;
@@ -80,12 +134,15 @@ const Home = () => {
             <Card accent="Info">
               <CardBody>
                 <img src={cardProp.thumbnail} />
+                <Button onClick={handleDownload}>
+                  Download from aws
+                </Button>
               </CardBody>
               <CardFooter>{cardProp.title}</CardFooter>
             </Card>
           </Col>
         ))}
-      </Row>,
+      </Row>
     );
   }
   return (
