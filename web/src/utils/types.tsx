7e682d79a93a5706ae7eb1f8d9a55f3fa5d42bb6
EricJLi <li.eric1998@gmail.com>
2022-03-06 01:14:54 -0500

    add edit page to website

---
+++ web/src/utils/types.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export interface CardPropsType {
+  title: string,
+  description: string,
+  thumbnail: string,
+  three_dimen_object_blob_storage: string,
+  objectname: string
+}
